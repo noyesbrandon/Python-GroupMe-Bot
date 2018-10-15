a3a4d52834e1041e4206f9ab27aef9cfe3ac22a1
Jacob Madian <44143326+JacobMadian@users.noreply.github.com>
2018-10-15 11:37:48 -0700

    New version including table output

--- GMBV6.py
+++ GMBV6.py
@@ -9,8 +9,8 @@ import datetime
 botCall = '!bot'
 helpCall = '!help'
 nameCall = '!names'
-request_params = {'token': "xGP1VqL0vAYKgBdNNE5az5MbLySz2e84wmLUJEJM"}
-
+request_params = {'token': open("C:/Users/JakeDesktop/Desktop/Token.txt","r")}
+with open("C:/Users/JakeDesktop/Desktop/Token.txt","r") as file: bot_token = file.read()
 
 help_message = """
 	Hey! To get my attention use this format!
@@ -23,28 +23,21 @@ help_message = """
 #These lists hold names of those who want leftovers saved for each meal. Set to be wiped after the cutoff for dinner.
 lunch_list = []
 dinner_list = []
-command_list = ['start']
-message_log = []
 initialize = 0
 last_message_id = ''
 
 #Used to make sure fake names or nicknames are not put onto the lists. Keeps things consistent for chef.
-unadjusted_member_list = ["Jacob", "Carson", "Brendon","Petra","Wasserman","Thomas","Phan", "Harrison","Anderson","Lauerman","Amir","Evan","Nikko","Andrew","Fragasso","Quinn","Austin","Bryce","Yuhao","Karim","Binns","Blake","Josh","Peyton","Robert","Kris","Gabe","Justin","Luke","Bryce","Will","Aidan","Garrett","BJ","Steve","Deven","YenWen","Matthew","Nico", "Nussbaum","Ben","Sam","Owen","Javier","Rodrigo","Phelan", "Ryan","Jared","Howard","Colton","Dustin","Ari","Ed","Lampkin","Robi","Zach","JHerbruger","Kevin","Aune","Paul","Will","Cole", "Kelvin","Hytrek","Spencer","Tim"]
+unadjusted_member_list = open("C:/Users/JakeDesktop/Desktop/Member_List.txt").read().splitlines()
 member_list = [entry.lower() for entry in unadjusted_member_list]
 
 #Adjustable times for cutoffs, depending on meal type and chef requests
-now = datetime.datetime.now()
-lunch_cutoff_time = now.replace(hour =10, minute = 39, second = 30, microsecond = 0)
-dinner_cutoff_time = now.replace(hour = 21, minute = 40, second = 0, microsecond = 0)	
-reset_time = now.replace(hour = 23, minute = 47, second = 00, microsecond = 0)
+lunch_cutoff_time = datetime.datetime.now().replace(hour =10, minute = 39, second = 30, microsecond = 0)
+dinner_cutoff_time = datetime.datetime.now().replace(hour = 23, minute = 40, second = 0, microsecond = 0)	
+reset_time = datetime.datetime.now().replace(hour = 23, minute = 47, second = 00, microsecond = 0)
 
 #Method called to push message to GroupMe chat
 def message_send (to_send):
-	request_params = {'token': "xGP1VqL0vAYKgBdNNE5az5MbLySz2e84wmLUJEJM"}
-	response = requests.get('https://api.groupme.com/v3/groups/43302265/messages', params = request_params)
-	if (response.status_code == 200):
-		response_messages = response.json()['response']['messages']
-		post_params = { 'bot_id' : '6f8d34b8f636d81ff35d7d283c', 'text': to_send } 
+	if last_response_code == 200:
 		requests.post('https://api.groupme.com/v3/bots/post', params = post_params)
 	
 def time_comparator (now, lunch_cutoff_time, dinner_cutoff_time,lastMessage):
@@ -111,7 +104,17 @@ while True:
 		lunch_list.clear()
 		dinner_list.clear()
 	
-	response_messages = requests.get('https://api.groupme.com/v3/groups/43302265/messages', params = {'token': "xGP1VqL0vAYKgBdNNE5az5MbLySz2e84wmLUJEJM"}).json()['response']['messages']
+	try:
+		response = requests.get('https://api.groupme.com/v3/groups/43302265/messages', params = {'token': "xGP1VqL0vAYKgBdNNE5az5MbLySz2e84wmLUJEJM"}).json()['response']['messages']
+		response_messages = response.json()['response']['messages']
+		last_response_code = response.status_code
+		try:
+			assert last_response_code = 200
+		except AssertionError:
+			print("Bad response Code: " + last_response_code)
+	except TypeError:
+		continue
+
 	if initialize == 0:
 		print('initialize')
 		initialize += 1
@@ -126,7 +129,8 @@ while True:
 				if lastMessage == '!help':
 					message_send(help_message)
 				elif lastMessage == '!names':
-					message_send(str(unadjusted_member_list))					
+					message_send(str(unadjusted_member_list[0:]))
+					print(str(unadjusted_member_list))					
 				else:
 					time_comparator(now, lunch_cutoff_time, dinner_cutoff_time, lastMessage)
 				#print(str(lastMessage + ' sent by ' + response_messages[0]['name']))
---
+++ GMBV7.py
@@ -0,0 +1,181 @@
+# GroupMeBot created by Jacob Madian
+# This program was created to automate the process of allocating leftover food in a house of 40 people
+# In the past, requests for leftover food have been made by writing your name on a whiteboard located in the kitchen
+# As people forget to sign up before they leave, a large amount of people consistently ask for somebody to write their name on the board
+# This code uses a GroupMe chat to automate this feature and display active lists for lunch and dinner requests
+# A raspberry pi is consistently running this code in the kitchen attached to a monitor for visual access, and resets all request lists at a specified time
+
+import requests
+import time
+import datetime
+import os
+from texttable import Texttable
+
+botCall = '!bot'
+helpCall = '!help'
+nameCall = '!names'
+clear = lambda: os.system('cls')
+with open("C:/Users/JakeDesktop/Desktop/Token.txt","r") as file: bot_token = file.read()
+request_params = {'token': bot_token}
+
+help_message = """
+	Hey! To get my attention use this format!
+
+	!bot meal full name
+
+	If you forget your assigned name, use !names PC(insert number here)
+
+	!names PC16
+
+	As an example
+	"""
+
+#These lists hold names of those who want leftovers saved for each meal. Set to be wiped after the cutoff for dinner.
+lunch_list = []
+dinner_list = []
+initialize = 0
+last_message_id = ''
+
+#Table creation and modification
+lunch_output_table = Texttable()
+dinner_output_table = Texttable()
+dinner_output_table.add_row([['        Dinner         ']])
+lunch_output_table.add_row([['         Lunch         ']])
+lunch_output_table.set_cols_align("c")
+dinner_output_table.set_cols_align("c")
+
+#Used to make sure fake names or nicknames are not put onto the lists. Keeps things consistent for chef.
+unadjusted_member_list = open("C:/Users/JakeDesktop/Desktop/Member_List.txt").read().splitlines()
+member_list = [entry.replace(" ","").lower() for entry in unadjusted_member_list]
+
+#List subdivision to avoid extremely long lists sent to chat
+PC18_names = unadjusted_member_list[:18]
+PC17_names = unadjusted_member_list[18:40]
+PC16_names = unadjusted_member_list[40:61]
+PC15_names = unadjusted_member_list[61:84]
+
+#Adjustable times for cutoffs, depending on meal type and chef requests
+lunch_cutoff_time = datetime.datetime.now().replace(hour =13, minute = 39, second = 30, microsecond = 0)
+dinner_cutoff_time = datetime.datetime.now().replace(hour = 23, minute = 40, second = 0, microsecond = 0)	
+reset_time = datetime.datetime.now().replace(hour = 23, minute = 47, second = 00, microsecond = 0)
+
+#Method called to push message to GroupMe chat
+def message_send (to_send):
+	if last_response_code == 200:
+		requests.post('https://api.groupme.com/v3/bots/post', params = { 'bot_id' : '6f8d34b8f636d81ff35d7d283c', 'text': to_send })
+
+#Sorts message to various conditions depending on time relative to cutoffs
+def time_comparator (now, lunch_cutoff_time, dinner_cutoff_time,lastMessage):
+	if now < lunch_cutoff_time:
+		string_parser(lastMessage)
+		#add to lunch list
+	elif (now > lunch_cutoff_time) & (now < dinner_cutoff_time):
+		if "!botlunch" in lastMessage.replace(" ","").lower():
+			message_send("You're to late for lunch plates! Check the fridge for leftovers and try again tomorrow!")
+		else:
+			string_parser(lastMessage)
+
+	elif now > dinner_cutoff_time:
+		message_send("You missed the last deadline! Check the fridge for leftovers and try again tomorrow!")
+	
+	else:
+		pass
+
+#Message creation and list appending, as well as table clearing and redrawing
+def string_parser(user_input):
+	text = user_input.replace(" ","").lower()
+	if "!botlunch" in text:
+		text = text[9:]
+		if text in member_list:
+			match_index = [i for i, s in enumerate(member_list) if text in s]
+			member_name = unadjusted_member_list[match_index[0]]
+			if member_name not in lunch_list:
+				lunch_list.append(member_name)
+				print(str('Current lunch list ' + str(lunch_list)))
+				lunch_output_table.add_row([[str(member_name)]])
+				clear()
+				print(lunch_output_table.draw())
+				print(dinner_output_table.draw())
+				message_send('Gotcha ' + member_name + ' you are on the lunch list!')
+			else:
+				message_send("You're already on the lunch list " + member_name +"!")
+		else:
+			message_send('Name not recognized, use !names if you forgot your assigned name!')
+	elif "!botdinner" in text:
+		text = text[10:]
+		if text in member_list:
+			match_index = [i for i, s in enumerate(member_list) if text in s]
+			member_name = unadjusted_member_list[match_index[0]]
+			if member_name not in dinner_list:
+				dinner_list.append(member_name)
+				print(str('Current dinner list ' + str(dinner_list)))
+				dinner_output_table.add_row([[str(member_name)]])
+				clear()
+				print(lunch_output_table.draw())
+				print(dinner_output_table.draw())
+				message_send('Gotcha ' + member_name + ' you are on the dinner list!')
+			else:
+				message_send("You're already on the dinner list " + member_name +"!")
+		else:
+			message_send('Name not recognized, use !names if you forgot your assigned name!')
+	else:
+		message_send("That's not a command, use !help if you don't remember the commands!")
+
+#Function sorts which grouping of names to send to avoid clutter in main code body
+def name_selection(user_input):
+	if "pc18" in user_input.lower():
+		message_send(str(PC18_names))
+	elif "pc17" in user_input.lower():
+		message_send(str(PC17_names))
+	elif "pc16" in user_input.lower():
+		message_send(str(PC16_names))
+	elif "pc15" in user_input.lower():
+		message_send(str(PC15_names))
+	else:
+		message_send("Try again but make sure to include PC with your year following it!")
+
+while True:
+	time.sleep(.5)
+	now = datetime.datetime.now()
+	if now > reset_time:
+		lunch_list.clear()
+		dinner_list.clear()
+		clear()
+	try:
+		response = requests.get('https://api.groupme.com/v3/groups/43302265/messages', params = {'token': bot_token})
+		response_messages = response.json()['response']['messages']
+		last_response_code = response.status_code
+		try:
+			assert last_response_code == 200
+		except AssertionError:
+			print("Bad response Code: " + last_response_code)
+	except TypeError:
+		continue
+
+	if initialize == 0:
+		print('initialize')
+		initialize += 1
+	else:
+		try:
+			message = response_messages[0]
+			latestId = message['id']
+			if message['name'] == 'ThotBot':
+				continue
+			if latestId != last_message_id:
+				last_message_id = latestId
+				lastMessage = message['text']
+				if lastMessage == '!help':
+					message_send(help_message)
+				elif "!names" in lastMessage.lower():
+					name_selection(lastMessage)
+				else:
+					time_comparator(now, lunch_cutoff_time, dinner_cutoff_time, lastMessage)
+		except(IndexError):
+			print('No Messages In Chat')
+
+	
+
+
+	
+
+
